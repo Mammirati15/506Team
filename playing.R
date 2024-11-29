019d7dcf04f3cb43212d9d2e2e5cbd4eb9aa8996
Matt Ammirati <Matt.ammirati@gmail.com>
2024-11-29 13:13:37 -1000

    first commit

---
+++ playing.R
@@ -0,0 +1,13 @@
+temperature <- c(40, 50, 60, 70, 80, 90)
+
+for(count in 1:6) {
+  cat('weather for the day', count, ":")
+  if(temperature[count]< 60){
+    print('terrible for a trip')
+  } else if (temperature[count > 60]{
+    print('time for some sex outside')
+  }) else {
+    print('what else is there')
+  }
+}
+
---
+++ sure.R
@@ -0,0 +1,14 @@
+# Define the number for multiplication
+number <- 5
+
+# Initialize an empty vector to store results
+results <- numeric(10)
+
+# Perform multiplication using a for loop
+for (i in 1:10) {
+  results[i] <- number * i
+}
+
+# Print the results
+print(results)
+
